7d64e413b2ceb019e5ef38485f5acd45a5cdc699
Marcello de Sales <Marcello_DeSales@intuit.com>
2017-04-14 00:21:35 -0700

    PropertySourcesPlaceholderConfigurer: Making @Value @ConfigurationProperties
    work requires adding this class with the bean creation that enables the
    resolution of properties with @Value annotation. This is the key piece
    of integrating exising Spring Framework apps and get the benefits for
    SpringBoot (Until you make time and budget for migration :d)

---
+++ src/main/java/com/zenika/controller/config/PropertiesConfigurer.java
@@ -0,0 +1,18 @@
+package com.zenika.controller.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import com.zenika.controller.GithubProperties;
+
+@Configuration
+@EnableConfigurationProperties({GithubProperties.class})
+public class PropertiesConfigurer {
+
+  @Bean
+  public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+    return new PropertySourcesPlaceholderConfigurer();
+  }
+}
